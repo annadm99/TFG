35a5ed27d6142210bd1fdc86096c5f64fa733ec9
annadotmariano <55254764+annadotmariano@users.noreply.github.com>
2021-10-06 21:01:59 +0200

    Lectura  asíncrona realitzada
    Implementada amb MultiPathStream. Proximament s'implementarà un únic listener per a tota una vitro.

--- MCU_config/MCU_config.ino
+++ MCU_config/MCU_config.ino
@@ -21,9 +21,50 @@
 
 
 
-FirebaseData fbdo;
+FirebaseData fb_readBalanca;
+String parentPathBalanca = "/user1/vitro2545/balan%C3%A7a";
+String childPathBalanca[2] = {"/on", "/pes"};
+
 FirebaseAuth auth;
 FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+void streamCallbackBalanca(MultiPathStream stream)
+{
+  size_t numChild = sizeof(childPathBalanca) / sizeof(childPathBalanca[0]);
+  
+
+  for (size_t i = 0; i < numChild; i++)
+  {
+    Serial.println("SEPARACION");
+    Serial.println(stream.get(childPathBalanca[i]));
+    //comprovar si ha canviat els fills
+    if (stream.get(childPathBalanca[i]))
+    {
+      Serial.printf("path: %s, event: %s, type: %s, value: %s%s", stream.dataPath.c_str(), stream.eventType.c_str(), stream.type.c_str(), stream.value.c_str(), i < numChild - 1 ? "\n" : "");
+    }
+  }
+
+  Serial.println();
+
+  //This is the size of stream payload received (current and max value)
+  //Max payload size is the payload size under the stream path since the stream connected
+  //and read once and will not update until stream reconnection takes place.
+  //This max value will be zero as no payload received in case of ESP8266 which
+  //BearSSL reserved Rx buffer size is less than the actual stream payload.
+  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", stream.payloadLength(), stream.maxPayloadLength());
+}
+
+void streamTimeoutCallbackBalanca(bool timeout)
+{
+  if (timeout)
+    Serial.println("stream timed out, resuming...\n");
+
+  if (!fb_readBalanca.httpConnected())
+    Serial.printf("error code: %d, reason: %s\n\n", fb_readBalanca.httpCode(), fb_readBalanca.errorReason().c_str());
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -49,7 +90,7 @@ void setup() {
   //per a reconnectar automaticament
   Firebase.reconnectWiFi(true);
   //size del buffer de la firebase a dwld
-  fbdo.setResponseSize(4096);
+  //fbdo.setResponseSize(4096);
 
   //per comprovar l'estat del tokken
   config.token_status_callback = tokenStatusCallback; 
@@ -57,12 +98,21 @@ void setup() {
   config.max_token_generation_retry = 5;
   /* Initialize the library with the Firebase authen and config */
   Firebase.begin(&config, &auth);
+  
+  //lectura asíncrona de la balança
+  if (!Firebase.RTDB.beginMultiPathStream(&fb_readBalanca, parentPathBalanca)){
+    Serial.printf("sream begin error, %s\n\n", fb_readBalanca.errorReason().c_str());
+  }
+  Firebase.RTDB.setMultiPathStreamCallback(&fb_readBalanca, streamCallbackBalanca, streamTimeoutCallbackBalanca);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Firebase.ready()){
-    Serial.println("hiii");
+  //cada quant s'executarà 
+  if(Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0) ){
+    //Serial.println("hiii");
+    sendDataPrevMillis = millis();
+    Firebase.RTDB.setMultiPathStreamCallback(&fb_readBalanca, streamCallbackBalanca, streamTimeoutCallbackBalanca);
   }
 
 }
